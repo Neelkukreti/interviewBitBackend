818da0b15af32b9559392e2affb86683a97d07f3
Neel <Neel.kukreti23@gmail.com>
2020-11-21 16:36:17 +0530

    code  cleanup

--- routes/api/interview.js
+++ routes/api/interview.js
@@ -19,24 +19,43 @@ router.get('/fetch', async (req, res) => {
   }
 });
 
+
+function timeClash(interview_start_time,interview_end_time,start_time ,end_time)
+{
+          if (
+            start_time <= interview_start_time &&
+            end_time >= interview_end_time
+          ) {
+            return true;
+          } else if (
+            start_time >= interview_start_time &&
+            end_time <= interview_end_time
+          ) {
+            return true;
+          } else if (
+            start_time <= interview_start_time &&
+            end_time <= interview_end_time &&
+            end_time >= interview_start_time
+          ) {
+            return true;
+          } else if (
+            start_time >= interview_start_time &&
+            start_time <= interview_end_time &&
+            end_time >= interview_end_time
+          ) {
+            return true;
+          }
+          else
+          return false;
+        
+      }
+
+
 router.post(
   '/create',
-  // [
-  //check('date', 'Date is req').not().isEmpty(),
-  //check('timeStart', 'time is not empty').not().isEmpty(),
-  //check('timeEnd', 'time is not empty').not().isEmpty(),
-  //check('candidate', 'candidate is not empty').not().isEmpty(),
-  //check('interviewer', 'interviewer is not empty').not().isEmpty(),
-  //check('interviewerName', 'interviewer Name is not empty').not().isEmpty(),
-  //check('candidateName', 'candidate Name is not empty').not().isEmpty(),
-  // ],
+ 
   async (req, res) => {
-    //  console.log(req.body);
-
-    //  const errors = validationResult(req);
-    //  if (!errors.isEmpty()) {
-    ////    return res.status(400).json({ errors: errors.array() });
-    // }
+    
 
     const {
       date,
@@ -50,57 +69,25 @@ router.post(
 
     try {
       //if candidate has an interview already
-      let interview = await Interview.find({ candidate });
+      let interview = await Interview.find({ candidate: candidate, date:date } );
       let newInterviewerList = interviewer.split(',');
       newInterviewerList = newInterviewerList.filter((i) => i != '');
 
       if (interview.length > 0) {
+        console.log(interview);
         for (let x in interview) {
-          if (interview[x].date == date) {
+        //  if (interview[x].date == date) {
             let interview_start_time = parseInt(interview[x].timeStart);
             let interview_end_time = parseInt(interview[x].timeEnd);
             let start_time = parseInt(timeStart);
             let end_time = parseInt(timeEnd);
-            if (
-              start_time <= interview_start_time &&
-              end_time >= interview_end_time
-            ) {
-              console.log(timeStart + ' S' + interview[x].timeStart);
-              return res.status(503).json({
-                response: 'Error',
-                msg: ' Candidate is Busy',
-              });
-            } else if (
-              start_time >= interview_start_time &&
-              end_time <= interview_end_time
-            ) {
-              console.log(timeStart + ' S' + interview[x].timeStart);
-              return res.status(503).json({
-                response: 'Error',
-                msg: ' Candidate is Busy',
-              });
-            } else if (
-              start_time <= interview_start_time &&
-              end_time <= interview_end_time &&
-              end_time >= interview_start_time
-            ) {
-              console.log(timeStart + ' S' + interview[x].timeStart);
-              return res.status(503).json({
-                response: 'Error',
-                msg: ' Candidate is Busy',
-              });
-            } else if (
-              start_time >= interview_start_time &&
-              start_time <= interview_end_time &&
-              end_time >= interview_end_time
-            ) {
-              console.log(timeStart + ' S' + interview[x].timeStart);
-              return res.status(503).json({
-                response: 'Error',
-                msg: ' Candidate is Busy',
-              });
-            }
-          }
+           if(timeClash(interview_start_time,interview_end_time,start_time ,end_time))
+           {
+            return res.status(503).json({
+              response: 'Error',
+              msg: 'Candidate is Busy',
+            });
+           }
         }
       }
       interview = await Interview.find({ date });
@@ -117,43 +104,11 @@ router.post(
                 let interview_end_time = parseInt(interview[x].timeEnd);
                 let start_time = parseInt(timeStart);
                 let end_time = parseInt(timeEnd);
-                if (
-                  start_time <= interview_start_time &&
-                  end_time >= interview_end_time
-                ) {
-                  console.log(timeStart + ' S' + interview[x].timeStart);
+                if(timeClash(interview_start_time,interview_end_time,start_time ,end_time))
+                {
                   return res.status(503).json({
                     response: 'Error',
-                    msg: interviwerList[z] + ' is Busy',
-                  });
-                } else if (
-                  start_time >= interview_start_time &&
-                  end_time <= interview_end_time
-                ) {
-                  console.log(timeStart + ' S' + interview[x].timeStart);
-                  return res.status(503).json({
-                    response: 'Error',
-                    msg: interviwerList[z] + ' is Busy',
-                  });
-                } else if (
-                  start_time <= interview_start_time &&
-                  end_time <= interview_end_time &&
-                  end_time >= interview_start_time
-                ) {
-                  console.log(timeStart + ' S' + interview[x].timeStart);
-                  return res.status(503).json({
-                    response: 'Error',
-                    msg: interviwerList[y] + ' In is Busy',
-                  });
-                } else if (
-                  start_time >= interview_start_time &&
-                  start_time <= interview_end_time &&
-                  end_time >= interview_end_time
-                ) {
-                  console.log(timeStart + ' S' + interview[x].timeStart);
-                  return res.status(503).json({
-                    response: 'Error',
-                    msg: interviwerList[y] + ' In is Busy',
+                    msg: interviwerList[y] + ' is Busy',
                   });
                 }
               }
@@ -208,23 +163,9 @@ router.post(
 
 router.post(
   '/update',
-  //  [
-  //  check('id', 'id is req').not().isEmpty(),
-  //  check('date', 'Date is req').not().isEmpty(),
-  //  check('timeStart', 'time is not empty').not().isEmpty(),
-  //  check('timeEnd', 'time is not empty').not().isEmpty(),
-  //  check('candidate', 'candidate is not empty').not().isEmpty(),
-  //  check('interviewer', 'interviewer is not empty').not().isEmpty(),
-  //  check('interviewerName', 'interviewer Name is not empty').not().isEmpty(),
-  //  check('candidateName', 'candidate Name is not empty').not().isEmpty(),
-  //],
 
   async (req, res) => {
-    //   const errors = validationResult(req);
-    //    if (!errors.isEmpty()) {
-    //     return res.status(400).json({ errors: errors.array() });
-    //  }
-    //
+
     console.log(req.body);
 
     const {
@@ -239,75 +180,41 @@ router.post(
     } = req.body;
 
     try {
-      //if candidate has an interview already
+      
       let interview = await Interview.findOne({ _id: id });
       let newInterviewerList = interviewer.split(',');
       newInterviewerList = newInterviewerList.filter((i) => i != '');
 
       if (interview) {
-        let interview = await Interview.find({ candidate });
+        let interview = await Interview.find({ candidate , date:date , _id:{$ne:id} });
         if (interview.length > 0) {
           for (let x in interview) {
-            if (interview[x].date == date && interview[x]._id != id) {
               let interview_start_time = parseInt(interview[x].timeStart);
               let interview_end_time = parseInt(interview[x].timeEnd);
               let start_time = parseInt(timeStart);
               let end_time = parseInt(timeEnd);
-              if (
-                start_time <= interview_start_time &&
-                end_time >= interview_end_time
-              ) {
-                console.log(timeStart + ' S' + interview[x].timeStart);
+              if(timeClash(interview_start_time,interview_end_time,start_time ,end_time))
+              {
                 return res.status(503).json({
                   response: 'Error',
-                  msg: ' Candidate is Busy',
-                });
-              } else if (
-                start_time >= interview_start_time &&
-                end_time <= interview_end_time
-              ) {
-                console.log(timeStart + ' S' + interview[x].timeStart);
-                return res.status(503).json({
-                  response: 'Error',
-                  msg: ' Candidate is Busy',
-                });
-              } else if (
-                start_time <= interview_start_time &&
-                end_time <= interview_end_time &&
-                end_time >= interview_start_time
-              ) {
-                console.log(timeStart + ' S' + interview[x].timeStart);
-                return res.status(503).json({
-                  response: 'Error',
-                  msg: ' Candidate is Busy',
-                });
-              } else if (
-                start_time >= interview_start_time &&
-                start_time <= interview_end_time &&
-                end_time >= interview_end_time
-              ) {
-                console.log(timeStart + ' S' + interview[x].timeStart);
-                return res.status(503).json({
-                  response: 'Error',
-                  msg: ' Candidate is Busy',
+                  msg: 'Candidate is Busy',
                 });
               }
+              
             }
           }
-        }
+        
 
-        interview = await Interview.find({ date });
+        interview = await Interview.find({ date: date , _id:{$ne:id} });
         if (interview.length > 0) {
           for (let x in interview) {
             console.log(interview[x]._id + ' ' + id);
-            if (!(String(interview[x].id) == String(id))) {
+            //if (!(String(interview[x].id) == String(id))) {
               console.log('IN');
               let interviwerList = interview[x].interviewer.split(',');
               interviwerList = interviwerList.filter((i) => i != '');
-
               for (let y in interviwerList) {
                 for (let z in newInterviewerList) {
-                  //  console.log("A"+interviwerList[y]+"B"+newInterviewerList[z]);
                   if (interviwerList[y] == newInterviewerList[z]) {
                     if (interviwerList[y] == newInterviewerList[z]) {
                       let interview_start_time = parseInt(
@@ -316,50 +223,18 @@ router.post(
                       let interview_end_time = parseInt(interview[x].timeEnd);
                       let start_time = parseInt(timeStart);
                       let end_time = parseInt(timeEnd);
-                      if (
-                        start_time <= interview_start_time &&
-                        end_time >= interview_end_time
-                      ) {
-                        console.log(timeStart + ' S' + interview[x].timeStart);
-                        return res.status(503).json({
-                          response: 'Error',
-                          msg: interviwerList[y] + ' is Busy',
-                        });
-                      } else if (
-                        start_time >= interview_start_time &&
-                        end_time <= interview_end_time
-                      ) {
-                        console.log(timeStart + ' S' + interview[x].timeStart);
-                        return res.status(503).json({
-                          response: 'Error',
-                          msg: interviwerList[y] + ' is Busy',
-                        });
-                      } else if (
-                        start_time <= interview_start_time &&
-                        end_time <= interview_end_time &&
-                        end_time >= interview_start_time
-                      ) {
-                        console.log(timeStart + ' S' + interview[x].timeStart);
-                        return res.status(503).json({
-                          response: 'Error',
-                          msg: interviwerList[y] + ' In is Busy',
-                        });
-                      } else if (
-                        start_time >= interview_start_time &&
-                        start_time <= interview_end_time &&
-                        end_time >= interview_end_time
-                      ) {
-                        console.log(timeStart + ' S' + interview[x].timeStart);
-                        return res.status(503).json({
-                          response: 'Error',
-                          msg: interviwerList[y] + ' In is Busy',
-                        });
-                      }
+                      if(timeClash(interview_start_time,interview_end_time,start_time ,end_time))
+                        {
+                              return res.status(503).json({
+                              response: 'Error',
+                              msg: interviwerList[y] + ' is Busy',
+                                    });
+                        }
                     }
                   }
                 }
               }
-            }
+            
           }
         }
 
